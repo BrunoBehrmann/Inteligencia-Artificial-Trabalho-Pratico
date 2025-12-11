5c2b26fc69285a49c245e59083d8e28b15b3daad
reginaldoso <reginaldoso@ufba.br>
2025-12-11 18:17:08 -0300

    update 3

--- projeto/controllers/youbot/youbot.py
+++ projeto/controllers/youbot/youbot.py
@@ -1,18 +1,13 @@
-from controller import Robot, Lidar, Camera
+from controller import Robot
 from base import Base
 from arm import Arm
 from gripper import Gripper
-import cv2
-import numpy as np
-
 
 class YouBotController:
     def __init__(self):
-        # Acessa o robô
         self.robot = Robot()
-
         self.time_step = int(self.robot.getBasicTimeStep())
-
+        
         self.base = Base(self.robot)
         self.arm = Arm(self.robot)
         self.gripper = Gripper(self.robot)
@@ -22,40 +17,11 @@ class YouBotController:
 
         self.lidar = self.robot.getDevice("lidar")
         self.lidar.enable(self.time_step)
-
-        while self.robot.step(32) != -1:
-            # Lê os dados do Lidar
-            lidar_data = self.lidar.getRangeImage()
-
-            # Lê as imagens da câmera
-            img = self.camera.getImage()
-
-            # Converte a imagem para OpenCV
-            img_cv = np.array(img).reshape(
-                (self.camera.getHeight(), self.camera.getWidth(), 4))
-            img_cv = cv2.cvtColor(img_cv, cv2.COLOR_BGRA2BGR)
-
-            # Função personalizada a ser implementada qu processa as imagens para detectar o objeto
-            # Exemplo: Usando um modelo pré-treinado para detecção
-            # objeto = detectar_objeto(img_cv)
-
-            # Função personalizada a ser implementada que calcula a posição do objeto
-            # com base nos dados do Lidar
-            # posicao = calcular_posicao(lidar_data, objeto)
-
-            # Função personalizada a ser implementada que controla o movimento do robô
-            # controlar_robo(robot, posicao)
-
-            # Atualiza o robô
-            self.robot.step(32)
-
+        
+        
     def run(self):
         raise NotImplementedError("This method should be implemented")
 
-
 if __name__ == "__main__":
     controller = YouBotController()
-    # controller.run()
-
-
-robot = Robot()
+    #controller.run()
